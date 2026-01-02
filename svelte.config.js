1696900f8ed0eb8cd74adadda29f5afa349fdf9b
mkinteh2012-glitch <mkinteh2012@gmail.com>
2026-01-01 16:32:20 -0800

    added netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
+
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		// adapter-auto only works for some hosts, 
+		// adapter-netlify is specific for Netlify
 		adapter: adapter()
 	}
 };
